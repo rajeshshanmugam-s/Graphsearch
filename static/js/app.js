a258db99cc3a4d3aacb755aa495dbe53e7460842
vignesh0208 <vigneshwaran@positivenaick.com>
2020-02-05 19:05:02 +0530

    Major: design fixes

--- static/js/app.js
+++ static/js/app.js
@@ -4,22 +4,24 @@ var detailInfo = document.getElementById("detail-info");
 var backBtn = document.getElementById("back-btn");
 var finalBtn = document.getElementById("final-btn");
 var i= 0;
-var myvar;
+var currentUploadValue = '';
 var currentUploadCSVId = ''
+var businessNames = []
+var isValidated = false
 
 $(window).on("load", function() {
     fileupload.style.display = "block";
 });
 
 backBtn.onclick = function() {
+    $('#businessNames').empty();
+    $('#file').val('');
+    currentUploadValue = ''
     detailInfo.style.display = "none";
     fileupload.style.display = "block";
 }
 
 finalBtn.onclick = function() {
-    var businessNames = []
-    var isValidated = false
-
     $('#businessNames').children('.row').each(function(e) {
         var currentPosition = e + 1
         var businessNameInArray = $('#businessTags-'+currentPosition).tagsinput('items')
@@ -65,9 +67,11 @@ finalBtn.onclick = function() {
             data: JSON.stringify(sendJSON),
             success: function(values) {
                 console.log(values)
+                detailInfo.style.display = "none";
             },
             error: function(error) {
-                alert("An error occured, please try again.", error);     
+                alert("An error occured, please try again.", error);
+                detailInfo.style.display = "none";
             }
         });
 
@@ -76,7 +80,8 @@ finalBtn.onclick = function() {
 
 }
 
-function businessNameTemplate(values) {
+function businessNameTemplate() {
+    var values= currentUploadValue.column_names
     for(var i = 0; i < values.length; i++) {
         $('#businessNames').append(`
             <div class="row">
@@ -123,17 +128,66 @@ $("#file").change(function() {
         processData: false,
         contentType: false,
         data: fd,
-        success: function(values) {
-
-            detail.style.display = "none";
-            detailInfo.style.display = "block"
-            currentUploadCSVId = values.id
-            businessNameTemplate(values.data.column_names)
-            $('.tagsIpt').tagsinput('add', 'some tag');
-
-        },
-        error: function() {
-            alert("An error occured, please try again.");     
+        xhr: function () {
+            var xhr = $.ajaxSettings.xhr();
+            if (xhr.upload) {
+                xhr.upload.addEventListener('progress', function(event) {
+                    var percent = 0;
+                    var position = event.loaded || event.position;
+                    var total = event.total;
+                    if (event.lengthComputable) {
+                        percent = Math.ceil(position / total * 100);
+                    }
+                    $(".progress-bar").css("width", + percent +"%");
+                    $("#status").text(percent +"%");
+                }, true);
+            }
+            return xhr;
         }
+    }).done(function (values) {
+        detail.style.display = "none";
+        detailInfo.style.display = "block";
+        currentUploadValue = values.data
+        currentUploadCSVId = values.id;
+        businessNameTemplate();
+        $('.tagsIpt').tagsinput('add', 'some tag');
+    }).fail(function (error) {
+        alert("An error occured, please try again.", error);
     });
 });
+
+$( function() {
+    var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    $( "#myInput" ).autocomplete({
+      source: availableTags
+    });
+});
+
+
+$("#myInput").keypress(() => {
+    $("#myInput").addClass("intro");
+    $("#ui-id-1").css("border-top", "0px");
+})
