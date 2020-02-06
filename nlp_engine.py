4ffbea475151df764f2020a8f6984c7c27b6db99
rajeshshanmugam <rajeshshanmugam@positivenaick.com>
2020-02-06 20:32:26 +0530

    Minor fix query lower case

--- nlp_engine.py
+++ nlp_engine.py
@@ -2,18 +2,15 @@ from snips_nlu import SnipsNLUEngine
 from snips_nlu.dataset import Dataset
 
 engine = SnipsNLUEngine()
-intents = Dataset.from_yaml_files('en', ['data/summa.yml'])
-engine.fit(intents.json)
 
-
-def intents_generator(query):
+def intents_generator(query, id):
+    intents = Dataset.from_yaml_files('en', ['Data/'+id+'.yml'])
+    # Fixme: Should not be fitted for each request
+    engine.fit(intents.json)
     input_data = engine.parse(query)
     intents = engine.get_intents(query)
     column_name = intents[0]['intentName']
-    print(column_name)
-    print(input_data)
-    print(intents)
     return input_data, intents, column_name
 
 
-intents_generator("what are the age in data")
+# intents_generator("what are the age in data")
