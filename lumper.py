515cf789d2b7b474b2c4ea909ee61b8765dec1b4
rajeshshanmugam <rajeshshanmugam@positivenaick.com>
2020-02-10 18:11:24 +0530

    minor: change in piechart response

--- lumper.py
+++ lumper.py
@@ -14,7 +14,7 @@ class DataLumper:
     '''
     def __init__(self, file_path):
         self.file_path = file_path
-        self.df = DataLumper.data_frame_loader(self)
+        # self.columns, self.df = DataLumper.data_frame_loader(self)
 
     def data_frame_loader(self):
         '''
@@ -26,6 +26,8 @@ class DataLumper:
         logger.debug("Dataframe created")
         df = self.null_handler(df)
         logger.info("After Null handler")
+        df.to_csv(self.file_path)
+        logger.info("Cleaned data frame is written under the file name {}".format(self.file_path))
         return df.columns, df
 
     def null_handler(self, df):
@@ -43,7 +45,7 @@ class DataLumper:
 
 
 
-    def data_type_explorer(self):
+    def data_type_explorer(self, df):
         '''
         Differentiates the columns in the data frame as Continous and Categorical values.
         :return: Continous and Categorical Values
@@ -51,29 +53,29 @@ class DataLumper:
         categorical_column = []
         other_columns = []
 
-        descrb_columns = self.df.describe().columns
+        descrb_columns = df.describe().columns
         logger.info("Columns in DF describe {}".format(descrb_columns))
-        df_columns = self.df.columns
+        df_columns = df.columns
         logger.info("DataFrame Columns {}".format(df_columns))
 
         for column in df_columns:
             # Checking the column values are not in monotonic and also no of Uniques in a
             # Column are not equal to Total no of Rows
-            if not self.df[column].is_monotonic and self.df.shape[0] != len(self.df[column].unique()):
+            if not df[column].is_monotonic and df.shape[0] != len(df[column].unique()):
                 logger.debug("Column is not monotonic and No of Unique value are less than No of Rows")
                 # if self.df[column].dtype == np.number:
                 #     print(column)
                 #     other_columns.append(column)
 
                 # Checking if column values are numeric and comparing with orginal values and converted float values.
-                if self.df[column].dtype == np.number and np.array_equal(self.df[column],
-                                                                         self.df[column].astype(float)):
+                if df[column].dtype == np.number and np.array_equal(df[column],
+                                                                         df[column].astype(float)):
                     logger.debug("Column type is Numeric and Float {}".format(column))
                     logger.info("Column added in Continuous category {}".format(column))
                     other_columns.append(column)
 
                 # Check for unique values of a column is less than 1/4 of the total rows
-                elif len(self.df[column].unique()) < len(self.df[column])/4:
+                elif len(df[column].unique()) < len(df[column])/4:
                     logger.debug("No of Unique value is less than 1/4th of the no of Rows")
                     logger.info("Column added in Categorical Data {}".format(column))
                     categorical_column.append(column)
@@ -90,9 +92,9 @@ class DataLumper:
                     logger.info("Column added to categorical column {}".format(column))
                     categorical_column.append(column)
                 # Checking max value of the column is 1 or 75th Percentile is 1 or 0 and checking for the column is int
-                elif (self.df[column].max() == 1 or np.percentile(self.df[column], 75, interpolation='midpoint') == 1
-                      or np.percentile(self.df[column], 75, interpolation='midpoint') == 0) and \
-                        np.array_equal(self.df[column], self.df[column].astype(int)):
+                elif (df[column].max() == 1 or np.percentile(df[column], 75, interpolation='midpoint') == 1
+                      or np.percentile(df[column], 75, interpolation='midpoint') == 0) and \
+                        np.array_equal(df[column], df[column].astype(int)):
                     logger.debug("column max value is 1 or 3rd Quartile value is 1 or 0, and the column is Integer")
                     logger.info("Column added to Category {}".format(column))
                     categorical_column.append(column)
